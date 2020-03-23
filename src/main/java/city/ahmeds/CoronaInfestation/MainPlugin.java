9ccb19b9c40e00b83ed09a0128fead1342b624cb
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 20:18:05 +0400

    Fixed duplicate createconfig in main class.

--- src/main/java/city/ahmeds/CoronaInfestation/MainPlugin.java
+++ src/main/java/city/ahmeds/CoronaInfestation/MainPlugin.java
@@ -26,23 +26,6 @@ public class MainPlugin extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
 
 	}
-	private void createConfig() {
-		try {
-			if (!getDataFolder().exists()) {
-				getDataFolder().mkdirs();
-			}
-			File file = new File(getDataFolder(), "config.yml");
-			if (!file.exists()) {
-				getLogger().info("Config.yml not found, creating!");
-				saveDefaultConfig();
-			} else {
-				getLogger().info("Config.yml found, loading!");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-	}
 
 
 	@Override
